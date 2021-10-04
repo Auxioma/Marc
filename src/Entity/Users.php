5f765dac9b637f260e63912888013b6ef76b6e78
chahir jbali <chahirjbali@gmail.com>
2021-10-04 22:07:35 +0100

    client pages

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -6,6 +6,7 @@ use App\Repository\UsersRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\JoinColumn;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -153,6 +154,7 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
 
     /**
      * @ORM\OneToOne(targetEntity=Horaires::class, cascade={"persist", "remove"})
+     * @JoinColumn(onDelete="SET NULL")
      */
     private $horaires;
 
